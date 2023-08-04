7321bb432b7aa37d604d60ba3c14dbe9f876ca79
Conrad NL <c.lekeaka.it2000@gmail.com>
2023-08-04 08:25:40 -0500

    cpackages.sh

---
+++ cpackages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#run this script with sudo access
+#run this script on a redhat linux system
+
+sudo yum install nano -y
+sudo yum install vim -y
+sudo yum install tree -y
+sudo yum install python nginx -y
+
